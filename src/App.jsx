7312b06609808cfdab737dbf7432000d9e33aff6
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2024-06-22 11:12:57 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <div>
+        coffee
+      </div>
+  )
+}
+
+export default App
